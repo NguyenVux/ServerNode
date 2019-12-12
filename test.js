11fffeda0a4335855caadaba08414d33a009de57
NguyenVux <rockmanx000@gmail.com>
2019-12-12 22:16:11 +0700

    Landing Page update

--- test.js
+++ test.js
@@ -1,10 +1,12 @@
 let express = require("express");
+let path = require("path");
 let app = express();
 
 let port = 80;
+let dir = path.join(__dirname,"static");
 app.get("/",(req,res)=>
 {
-  res.send("hello");
+  res.sendFile(path.join(dir,"index.html"));
 });
 
 app.listen(port,()=> console.log("Listen at port: "+port));

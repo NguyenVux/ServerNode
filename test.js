b1d691a0d6145b43482ee24d1e8fe31e67e57aaa
ViktorNguyen <rockmanx000@outlook.com>
2019-12-13 00:49:00 +0000

    Backend fix

--- test.js
+++ test.js
@@ -3,10 +3,11 @@ let path = require("path");
 let app = express();
 
 let port = 80;
-let dir = path.join(__dirname,"static");
+let dir = path.join(__dirname,`static//`);
+app.use(express.static(path.join(__dirname,"static/"),{index:"true"}));
 app.get("/",(req,res)=>
 {
-  res.sendFile(path.join(dir,"index.html"));
+  res.sendFile(path.join(dir));
 });
 
 app.listen(port,()=> console.log("Listen at port: "+port));

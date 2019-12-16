c035da4d857cbc3e8574c8a6f7da27b7c8fb46af
NguyenVux <rockmanx000@gmail.com>
2019-12-16 12:20:07 +0700

    Services page

--- test.js
+++ test.js
@@ -1,13 +1,20 @@
 let express = require("express");
 let path = require("path");
 let app = express();
-
 let port = 80;
 let dir = path.join(__dirname,`static//`);
 app.use(express.static(path.join(__dirname,"static/"),{index:"true"}));
 app.get("/",(req,res)=>
 {
-  res.sendFile(path.join(dir));
+  console.log("Some one connected");
+  res.render("index.ejs");
 });
+
+app.get("/services",(req,res)=>
+{
+  console.log("Some one connected");
+  res.render("services.ejs");
+});
+
 
 app.listen(port,()=> console.log("Listen at port: "+port));

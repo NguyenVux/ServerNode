ed05806bd87f493c65962d10fcd25cfa480ff6e6
NguyenVux <rockmanx000@gmail.com>
2019-12-17 21:20:50 +0700

    back END FIXE PLS

--- test.js
+++ test.js
@@ -2,24 +2,25 @@ let express = require("express");
 let path = require("path");
 let app = express();
 let port = 80;
-let dir = path.join(__dirname,`static//`);
+
+
+
+app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname,"static/"),{index:"true"}));
+
+
+
 app.get("/",(req,res)=>
 {
   console.log(`Processing Request to Services /`);
   res.render("index.ejs");
 });
 
-app.get("/services",(req,res)=>
+app.get("/:page",(req,res)=>
 {
-  console.log("Processing Request to Services");
-  res.render("servicesShow.ejs");
+  let file = req.params["page"] + ".ejs";
+  console.log(`Processing Request to ${req.params["page"]}`);
+  res.render(file);
 });
 
-
-app.get("/projects",(req,res)=>
-{
-  console.log("Processing Request to projects");
-  res.render("projects.ejs");
-})
 app.listen(port,()=> console.log("Listen at port: "+port));
